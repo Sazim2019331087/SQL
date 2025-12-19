94a7ec3198c5035a2a9ba07defdf6ebc4bc3c380
Md. Sazim Mahmudur Rahman <70155321+Sazim2019331087@users.noreply.github.com>
2025-12-19 18:35:33 +0600

    1327. List the Products Ordered in a Period

---
+++ 1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,96 @@
+# Problem statement:
+  --------------------
+Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+Return the result table in any order.
+The result format is in the following example.
+  
+# Table schema:
+-----------------
+Table: Products
+-----------------
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| product_id       | int     |
+| product_name     | varchar |
+| product_category | varchar |
++------------------+---------+
+product_id is the primary key (column with unique values) for this table.
+This table contains data about the companys products.
+ 
+
+Table: Orders
+-----------------
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| order_date    | date    |
+| unit          | int     |
++---------------+---------+
+This table may have duplicate rows.
+product_id is a foreign key (reference column) to the Products table.
+unit is the number of products ordered in order_date.
+  
+# Input:
+  -------------------------
+Products table:
++-------------+-----------------------+------------------+
+| product_id  | product_name          | product_category |
++-------------+-----------------------+------------------+
+| 1           | Leetcode Solutions    | Book             |
+| 2           | Jewels of Stringology | Book             |
+| 3           | HP                    | Laptop           |
+| 4           | Lenovo                | Laptop           |
+| 5           | Leetcode Kit          | T-shirt          |
++-------------+-----------------------+------------------+
+Orders table:
++--------------+--------------+----------+
+| product_id   | order_date   | unit     |
++--------------+--------------+----------+
+| 1            | 2020-02-05   | 60       |
+| 1            | 2020-02-10   | 70       |
+| 2            | 2020-01-18   | 30       |
+| 2            | 2020-02-11   | 80       |
+| 3            | 2020-02-17   | 2        |
+| 3            | 2020-02-24   | 3        |
+| 4            | 2020-03-01   | 20       |
+| 4            | 2020-03-04   | 30       |
+| 4            | 2020-03-04   | 60       |
+| 5            | 2020-02-25   | 50       |
+| 5            | 2020-02-27   | 50       |
+| 5            | 2020-03-01   | 50       |
++--------------+--------------+----------+
+
+# Output:
+  -------------------------
++--------------------+---------+
+| product_name       | unit    |
++--------------------+---------+
+| Leetcode Solutions | 130     |
+| Leetcode Kit       | 100     |
++--------------------+---------+
+
+# Solution:
+  --------------------------
+WITH Feb_Orders AS (
+    SELECT product_id ,
+    order_date ,
+    unit 
+    FROM Orders
+    WHERE 
+    Extract(MONTH FROM order_date) = 2
+    AND 
+    Extract(YEAR FROM order_date) = 2020
+)
+, Sum_Orders AS (
+    SELECT product_id , SUM(unit) as unit
+    FROM Feb_Orders
+    GROUP BY product_id
+)
+SELECT P.product_name ,
+nvl(SO.unit,0) as unit
+FROM Products P
+LEFT JOIN Sum_Orders SO
+ON P.product_id = SO.product_id
+WHERE SO.unit >= 100
